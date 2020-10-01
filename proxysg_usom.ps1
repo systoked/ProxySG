2d34754e849e7af8229a4fddb5ef26cfa44d773b
systoked <71460680+systoked@users.noreply.github.com>
2020-10-01 15:36:37 +0300

    Update proxysg_usom.ps1

--- proxysg_usom.ps1
+++ proxysg_usom.ps1
@@ -1,17 +1,21 @@
-﻿Remove-Item c:\proxysg\proxysgdb.txt
+
+Remove-Item c:\proxysg\proxysgdb.txt
 Invoke-WebRequest https://www.usom.gov.tr/url-list.txt -OutFile c:\proxysg\usomlist.txt
+$lines = @()
 $list =  Get-ChildItem -Path c:\proxysg\usomlist.txt
-gc $list| sort | get-unique > $lines
-$lines =  Get-ChildItem -Path c:\proxysg\usomuniqe.txt
+$lines += gc $list| sort | Get-Unique
+New-Item c:\proxysg\usomuniqe.txt -type File
+Add-Content -Path c:\proxysg\usomuniqe.txt -Value $lines
+$uniqelist =  Get-ChildItem -Path c:\proxysg\usomuniqe.txt
 
 $regex = ‘\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b’
 New-Item c:\proxysg\all.txt -type File
 New-Item c:\proxysg\ipout.txt -type File
-New-Item c:\proxysg\urlout.txt -type File
+New-Item c:\proxysg\urlout1.txt -type File
 $ipout = Get-ChildItem -Path C:\proxysg\ipout.txt
-$urlout = Get-ChildItem -Path C:\proxysg\urlout.txt
-$ips = (Select-String -path $lines  -Pattern $regex -AllMatches) |  % { $_.Matches } | % { $_.Value }
-$urls =  (Select-String -path $lines  -Pattern $regex -NotMatch ) | select -ExpandProperty line
+$urlout = Get-ChildItem -Path C:\proxysg\urlout1.txt
+$ips = (Select-String -path $uniqelist  -Pattern $regex -AllMatches) |  % { $_.Matches } | % { $_.Value }
+$urls =  (Select-String -path $uniqelist  -Pattern $regex -NotMatch ) | select -ExpandProperty line
 
 
 
@@ -32,23 +36,34 @@ foreach($url in $urls)
       {
         Add-Content -Path $urlout -Value ($split.Split('$?')[0])
       }
-      Add-Content -Path  $urlout  -Value 'end'
+
     }
 
-gc C:\proxysg\ipout.txt| sort | get-unique > C:\proxysg\ipunq.txt
-gc C:\proxysg\urlout.txt| sort | get-unique > C:\proxysg\urlunq.txt
+#uniqe IP
+$iplines = @()
+$iplist =  Get-ChildItem -Path c:\proxysg\ipout.txt
+$iplines += gc $iplist| sort | Get-Unique
+New-Item c:\proxysg\ipuniqe.txt -type File
+Add-Content -Path c:\proxysg\ipuniqe.txt -Value $iplines
+
+#uniqe URL
+$urllines = @()
+$urllist =  Get-ChildItem -Path c:\proxysg\urlout1.txt
+$urllines += gc $urllist| sort | Get-Unique
+New-Item c:\proxysg\urluniqe.txt -type File
+Add-Content -Path c:\proxysg\urluniqe.txt -Value $urllines
 
 Add-Content -Path C:\proxysg\proxysgdb.txt -Value  ('define category "USOM_IP"')
-Get-content -Path c:\proxysg\ipunq.txt | Add-Content -Path C:\proxysg\proxysgdb.txt  
+Get-content -Path c:\proxysg\ipuniqe.txt | Add-Content -Path C:\proxysg\proxysgdb.txt  
 Add-Content -Path  C:\proxysg\proxysgdb.txt  -Value 'end'
 Add-Content -Path C:\proxysg\proxysgdb.txt -Value('define category "USOM_URL"')
-Get-content -Path c:\proxysg\urlunq.txt | Add-Content -Path C:\proxysg\proxysgdb.txt  
+Get-content -Path c:\proxysg\urluniqe.txt | Add-Content -Path C:\proxysg\proxysgdb.txt  
 Add-Content -Path  C:\proxysg\proxysgdb.txt  -Value 'end'
 
 Remove-Item c:\proxysg\usomuniqe.txt
 Remove-Item c:\proxysg\all.txt
 Remove-Item c:\proxysg\usomlist.txt
 Remove-Item c:\proxysg\ipout.txt
-Remove-Item c:\proxysg\urlout.txt
-Remove-Item c:\proxysg\ipunq.txt
-Remove-Item c:\proxysg\urlunq.txt
+Remove-Item c:\proxysg\urlout1.txt
+Remove-Item c:\proxysg\ipuniqe.txt
+Remove-Item c:\proxysg\urluniqe.txt
