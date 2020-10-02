2a711d876c17f6ffe4c04bbbab4a482a2105a4f1
systoked <71460680+systoked@users.noreply.github.com>
2020-10-02 15:16:36 +0300

    Update proxysg_usom.ps1

--- proxysg_usom.ps1
+++ proxysg_usom.ps1
@@ -1,7 +1,7 @@
-
 If (Test-Path -Path c:\proxysg\proxysgdb.txt ) {
 Remove-Item c:\proxysg\proxysgdb.txt
 }
+$lines=@()
 Invoke-WebRequest https://www.usom.gov.tr/url-list.txt -OutFile c:\proxysg\usomlist.txt
 $list =  Get-ChildItem -Path c:\proxysg\usomlist.txt
 $lines += $list | sort | Get-Unique
@@ -9,6 +9,7 @@ $lines += $list | sort | Get-Unique
 $regex = ‘\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b’
 $ipout = @()
 $urlout =@()
+$urlout1 =@()
 $ips = $($lines | Select-String -Pattern $regex -AllMatches) |  % { $_.Matches } | % { $_.Value }
 $urls = $($lines | Select-String -Pattern $regex -NotMatch ) | select -ExpandProperty line
 
@@ -34,6 +35,17 @@ foreach($url in $urls)
       }
 
     }
+    foreach($url1 in $urlout)
+    {
+    if($url1.Contains('://')){
+      #$split1 = ($url1.Split('://')[0])
+      #$urlout1 +=($url1.Split('://')[0])
+      $splithttp = $url1 -split '://'
+      $urlout1 += $splithttp[1]}
+      else{
+      $urlout1 += $url1}
+}
+
 
 
 #uniqe IP
@@ -43,7 +55,8 @@ $iplines += $ipout| sort | Get-Unique
 
 #uniqe URL
 $urllines = @()
-$urllines += $urlout| sort | Get-Unique
+#$urllines += $urlout| sort | Get-Unique
+$urllines += $urlout1| sort | Get-Unique
 
 
 Add-Content -Path C:\proxysg\proxysgdb.txt -Value  ('define category "USOM_IP"')
@@ -52,4 +65,4 @@ Add-Content -Path  C:\proxysg\proxysgdb.txt  -Value 'end'
 Add-Content -Path C:\proxysg\proxysgdb.txt -Value('define category "USOM_URL"')
 Add-Content -Path C:\proxysg\proxysgdb.txt  -Value $urllines
 Add-Content -Path  C:\proxysg\proxysgdb.txt  -Value 'end'
-Remove-Item c:\proxysg\usomlist.txtx
+Remove-Item c:\proxysg\usomlist.txt
