d8c62aee7c5e09c5ed1d4029e9963ccbc89154ac
systoked <71460680+systoked@users.noreply.github.com>
2021-04-16 10:59:39 +0300

    Update run_proxysgusom.ps1

--- run_proxysgusom.ps1
+++ run_proxysgusom.ps1
@@ -1,3 +1,8 @@
+<##############################################
+#######  contact@sinatabanlioglu.com  #########
+##############################################>
+
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/systoked/ProxySG/master/proxysg_usom.ps1
