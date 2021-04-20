5717373fb391c7321116b977e02d96d817bd15a4
systoked <71460680+systoked@users.noreply.github.com>
2021-04-20 18:11:01 +0300

    Update proxysg_usom.ps1

--- proxysg_usom.ps1
+++ proxysg_usom.ps1
@@ -1,7 +1,3 @@
-<##############################################
-#######  contact@sinatabanlioglu.com  #########
-##############################################>
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 $lines=@()
